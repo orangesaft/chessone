bcacf9d1f6de0364268846b256718f2f9f24be3f
orangesaft <156236994+orangesaft@users.noreply.github.com>
2024-12-05 17:11:11 +0900

    commit

--- script.rb
+++ script.rb
@@ -1,3 +1,15 @@
+#take initial position, and final position
+#count how far it has to travel both horizontally and vertically
+#use the fact that we always have to travel either 2 horizontally AND 1 vertically or 1 horizontally AND 2 vertically by the rule
+#we can have both positive and negative values, to the right and to the left/ upward and downward
+#divide by 2 unil the remainder is either 1 or 0// two equations like -> 2h + 1v = C, 1h + 2v = D// or maybe just don't 
+## H = 2 // FH = 4
+# V = 3 + 0.5 // FV = 7
+
+#2a + 1b = FH
+#1a + 2b = FV 
+#2^2^2/ 2left/right * 1up/down, 2up/down * 1left/right
+
 def knight_moves(initial_pos,final_pos)
     
   x, y = initial_pos
